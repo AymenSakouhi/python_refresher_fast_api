fd0dc27db200f96fd9ca4fce6c02bfb8aa7dee7d
Aymen <aymenmarketer@gmail.com>
2025-10-04 21:50:22 +0200

    checking a vid of google, so why not solve it on the way

--- test_comparing_js_to_py.js
+++ test_comparing_js_to_py.js
@@ -10,3 +10,33 @@ console.log(arr);
 
 const random = arr[Math.ceil(Math.random() * arr.length - 1)];
 console.log(random);
+// google problem that I got from a video to solve for no reason and then I will complete the challenge to learn python
+class My_Array {
+  constructor(integers) {
+    this.integers = integers;
+  }
+
+  insertValue(int) {
+    this.integers.push(int);
+    console.log(`new integers: `, this.integers);
+  }
+
+  removeValue(int) {
+    const temp = this.integers;
+    const index = temp.indexOf(int);
+    temp.splice(index, 1);
+    this.integers = temp;
+    console.log(`new integers: ${this.integers} or ${temp}`);
+  }
+
+  getRandomValue() {
+    const random = Math.floor(Math.random() * (this.integers.length - 1));
+    return this.integers[random];
+  }
+}
+
+const temp = new My_Array([1, 2, 3, 4, 5]);
+temp.insertValue(6);
+temp.removeValue(4);
+console.log(temp.getRandomValue());
+// solved this in 5 mins. the guy in the vid is just too much
