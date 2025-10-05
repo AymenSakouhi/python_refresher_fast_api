11cfa794a077d1721efd8cd8930dda4cf4eb7046
Aymen <aymenmarketer@gmail.com>
2025-10-05 19:51:26 +0200

    getting a db connection working and doing the first route

--- FastAPI/todoapp/app/main.py
+++ FastAPI/todoapp/app/main.py
@@ -1,9 +1,40 @@
 """SQLALCHEMY tutorial on how to build an app"""
 
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import session
+
 import app.models as models
-from app.database import engine
+from app.models import Todos
+from app.database import engine, sessionLocal
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    """
+    Dependency that provides a SQLAlchemy database session.
+
+    Yields:
+        db (Session): SQLAlchemy database session.
+    Closes the session after the request is finished.
+    """
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Define a reusable type annotation for the database session dependency.
+# This uses Python's `Annotated` to combine the type (session) with the FastAPI dependency injection (`Depends(get_db)`).
+db_annotation = Annotated[session, Depends(get_db)]
+
+
+@app.post("/")
+async def read_all(db: db_annotation):
+    """reading all from sqlite3 db"""
+    return db.query(Todos).all()
